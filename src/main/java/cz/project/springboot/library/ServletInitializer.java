4370025ed4e694757cdbf38657c576f3df691f35
Alexander Mironov <mironov@datalite.cz>
2017-11-08 23:26:55 +0100

    Setting -> nastaveni projektu. Oprava ServletInitializer.java

--- src/main/java/cz/project/springboot/library/ServletInitializer.java
+++ src/main/java/cz/project/springboot/library/ServletInitializer.java
@@ -1,13 +1,13 @@
 package cz.project.springboot.library;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@SpringBootApplication
+@EnableAspectJAutoProxy // включаем использование AspectJ
+@ComponentScan(basePackages = {"cz.project.springboot.library"})
 public class ServletInitializer extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(LibraryApplication.class);
-	}
 
 }
